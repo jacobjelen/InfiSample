0363ae0d1b62df16ba676b06df2b6b6cd0b47ec6
jacobjelen <jacobjelen@gmail.com>
2022-01-26 21:37:04 +0100

    ADDED: selecting port from dropdown menu

---
+++ myScripts/mySerial.js
@@ -0,0 +1,52 @@
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+// let port = new SerialPort('/dev/cu.usbmodem14201', { baudRate: 9600 })
+// let parser = port.pipe(new Readline({ delimiter: '\n' }))
+let port, parser
+setPort('/dev/cu.usbmodem14201')
+
+// PortSelect // Populate the dropdown menu
+SerialPort.list().then(ports => {
+  console.log(ports)
+  const dropdown = document.getElementById('portSelect')
+
+  ports.forEach((port) => {
+    dropdown.add(new Option(port.path))
+  })
+})
+
+// FUNCTIONS ///////////////////////////////////////////////////////////////
+
+// Change port based on user selection
+function changePort () {
+  const selectedPort = document.getElementById('portSelect').value
+
+  if (port.isOpen) {
+    console.log(1)
+    port.close(() => {
+      console.log(2)
+      setPort(selectedPort)
+    })
+  } else {
+    setPort(selectedPort)
+  }
+}
+
+// set port
+function setPort (newPort) {
+  port = new SerialPort(newPort, { baudRate: 9600 })
+
+  port.on('open', () => {
+    console.log('serial port open')
+    port.flush()
+
+    // update parser object
+    parser = port.pipe(new Readline({ delimiter: '\n' }))
+    parser.on('data', data => {
+    // console.log('got word from arduino:', data);
+      document.getElementById('title').innerHTML = data
+    })
+  })
+
+  console.log(port.path + ' open: ' + port.isOpen)
+}
