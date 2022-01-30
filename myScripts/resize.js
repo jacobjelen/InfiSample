9d6672752da31046f57ab818487b163f4132c485
jacobjelen <jacobjelen@gmail.com>
2022-01-30 12:29:38 +0100

    ADDED: font files FIXED: - layout in testLayout2.html and testStyles2.css - scaling in resize.js 	- fully responsive with font size, border weight etc

--- myScripts/resize.js
+++ myScripts/resize.js
@@ -3,8 +3,9 @@ window.addEventListener('resize', resizeMain)
 
 function resizeMain () {
   // initial size = ratio to maintain
-  const contWidht = 400
-  const contHeight = 200
+  const contWidht = 900
+  const contHeight = 666
+  const multiplier = 0.9 // instead of margin. portion of the width or height filled by container
 
   const cont = document.getElementById('container')
 
@@ -12,23 +13,31 @@ function resizeMain () {
   const windowWidth = window.visualViewport.width
   const windowHeight = window.visualViewport.height
 
-  const multiplier = 0.8 // instead of margin. portion of the width or height filled by container
   const winRatio = windowWidth / windowHeight
   const contRatio = contWidht / contHeight
   let newWidth, newHeight
 
   if (contRatio >= winRatio) {
     // window WIDTH deciding the size
-    cont.style.backgroundColor = 'green'
+    // cont.style.backgroundColor = 'green'
     newWidth = windowWidth * multiplier
     newHeight = newWidth / contRatio
   } else {
     // window HEIGHT deciding the size
-    cont.style.backgroundColor = 'blue'
+    // cont.style.backgroundColor = 'blue'
+
     newHeight = windowHeight * multiplier
     newWidth = newHeight * contRatio
   }
 
   cont.style.width = newWidth + 'px'
   cont.style.height = newHeight + 'px'
+
+  document.documentElement.style.setProperty('--border-width', newWidth * 0.002 + 'px')
+  document.documentElement.style.setProperty('--font-size', newWidth * 0.03 + 'px')
+  // document.documentElement.style.setProperty('--main-color', 'pink')
+  // document.body.style.fontSize = newWidth * 0.03 + 'px'
+  // document.body.style.borderWidth = newWidth + 'px'
+
+//   cont.style.display = 'block'
 }
